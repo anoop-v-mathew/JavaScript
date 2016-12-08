e3efb480358d6fde4da88ab424fdfc5f00a1dba0
Anoop Varughese Mathew <anoop.v.mathew@gmail.com>
2016-12-08 17:29:46 +0300

    After beautifying code through JSHint and JSLint

--- factorial/scripts/factorial.js
+++ factorial/scripts/factorial.js
@@ -1,13 +1,21 @@
 function factorial(arg) {
 
-	// check if argument is boolean as Number() converts boolean to number
-	if (typeof(arg) == 'boolean') return NaN;
+    "use strict";
 
-	var n = Number(arg); // convert argument to number
+    var f, n = 0;
 
-	if (Number.isInteger(n) && (n >= 0)) { // factorial is valid only for non-negative integers
-		return (n == 0) ? 1 : n*factorial(n-1); // return 1 if argument is 0, else recurse
-	} else {
-		return NaN; // factorial cannot be determined for negative numbers
-	}
+    // check if argument is boolean as Number() converts boolean to number
+    if (typeof arg === "boolean") {
+        n = NaN;
+    } else {
+        n = Number(arg); // convert argument to number
+    }
+
+    if (Number.isInteger(n) && (n >= 0)) { // factorial is valid only for non-negative integers
+        f = (n === 0) ? 1 : n * factorial(n - 1); // return 1 if argument is 0, else recurse
+    } else {
+        f = NaN;
+    }
+
+    return f;
 }
