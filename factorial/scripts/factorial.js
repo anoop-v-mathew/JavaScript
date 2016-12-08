b4b295fc8f9b86f8930514422ba5890e0666cbde
Anoop Varughese Mathew <anoop.v.mathew@gmail.com>
2016-12-08 17:52:59 +0300

    More beautification

--- factorial/scripts/factorial.js
+++ factorial/scripts/factorial.js
@@ -1,3 +1,5 @@
+/* exported factorial */
+
 function factorial(arg) {
 
     "use strict";
@@ -11,10 +13,12 @@ function factorial(arg) {
         n = Number(arg); // convert argument to number
     }
 
-    if (Number.isInteger(n) && (n >= 0)) { // factorial is valid only for non-negative integers
-        f = (n === 0) ? 1 : n * factorial(n - 1); // return 1 if argument is 0, else recurse
+    // factorial is valid only for non-negative integers
+    if (Number.isInteger(n) && (n >= 0)) { 
+        // return 1 if argument is 0, else recurse
+        f = (n === 0) ? 1 : n * factorial(n - 1); 
     } else {
-        f = NaN;
+        f = NaN;	
     }
 
     return f;
