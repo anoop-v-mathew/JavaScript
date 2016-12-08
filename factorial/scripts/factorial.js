42395950073e34686b06bbe5a75e4dbe879cb402
Anoop Varughese Mathew <anoop.v.mathew@gmail.com>
2016-12-08 15:02:53 +0300

    changed scope of n from global to local

--- factorial/scripts/factorial.js
+++ factorial/scripts/factorial.js
@@ -3,8 +3,8 @@ function factorial(s) {
 	// check if argument is boolean as Number() converts boolean to number
 	if (typeof(s) == 'boolean') return NaN;
 
-	n = Number(s); // convert string to number
-	
+	var n = Number(s); // convert string to number
+
 	if (Number.isInteger(n) && (n >= 0)) { // factorial is valid only for non-negative integers
 		return (n == 0) ? 1 : n*factorial(n-1); // return 1 if argument is 0, else recurse
 	} else {
