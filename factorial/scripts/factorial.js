8d41dacf58246e28a1a2b566b46fc146df7f8bb5
Anoop Varughese Mathew <anoop.v.mathew@gmail.com>
2016-12-08 14:08:14 +0300

    formatting

--- factorial/scripts/factorial.js
+++ factorial/scripts/factorial.js
@@ -4,9 +4,10 @@ function factorial(s) {
 	if (typeof(s) == 'boolean') return NaN;
 
 	n = Number(s); // convert string to number
+	
 	if (Number.isInteger(n) && (n >= 0)) { // factorial is valid only for non-negative integers
-			return (n == 0) ? 1 : n*factorial(n-1); // return 1 if argument is 0, else recurse
-		} else {
-			return NaN; // factorial cannot be determined for negative numbers
-		}
+		return (n == 0) ? 1 : n*factorial(n-1); // return 1 if argument is 0, else recurse
+	} else {
+		return NaN; // factorial cannot be determined for negative numbers
+	}
 }
