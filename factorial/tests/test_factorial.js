b41f16bf8310389a9da25677b99ff854f4769444
Anoop Varughese Mathew <anoop.v.mathew@gmail.com>
2016-12-10 18:31:43 +0300

    Refactored unit test cases

--- factorial/tests/test_factorial.js
+++ factorial/tests/test_factorial.js
@@ -3,50 +3,26 @@
 /* global QUnit */
 /* global factorial */
 
-QUnit.test( "Factorial of 0 returns 1", function( assert ) {
-  assert.ok( factorial(0) == 1, "Passed!" );
+QUnit.test( "Positive Test Cases - factorial calculated correctly for these numbers", function( assert ) {
+  assert.ok( factorial(0) === 1, "Passed! - Factorial of 0 returns 1" );
+  assert.ok( factorial(5) === 120, "Passed! - Factorial of 5 returns 120" );
+  assert.ok( factorial(5.0) === 120, "Passed! - Factorial of 5.0 returns 120" );  
+  assert.ok( factorial(27) === 10888869450418352160768000000, "Passed! - Factorial of 27 returns 10888869450418352160768000000" );  
 });
 
-QUnit.test( "Factorial of 5 returns 120", function( assert ) {
-  assert.ok( factorial(5) == 120, "Passed!" );
+QUnit.test( "Negative Test Case - factorial value higher than what can be stored with precision as number", function( assert ) {
+  assert.ok( factorial(28) !== 304888344611713860501504000000, "Passed! - Factorial of 28 fails as it does not return 304888344611713860501504000000" );
 });
 
-QUnit.test( "Factorial of 27 returns 10888869450418352160768000000", function( assert ) {
-  assert.ok( factorial(27) == 10888869450418352160768000000, "Passed!" );
+QUnit.test( "Negative Test Cases - factorial is undefined for these numbers", function( assert ) {
+  assert.ok( isNaN(factorial(-1)), "Passed! - Factorial of -1 returns NaN" );
+  assert.ok( isNaN(factorial(5.5)), "Passed! - Factorial of 5.5 returns NaN" );  
+  assert.ok( isNaN(factorial(-5.5)), "Passed! - Factorial of -5.5 returns NaN" );  
 });
 
-QUnit.test( "Factorial of 28 fails as it does not return 304888344611713860501504000000", function( assert ) {
-  assert.ok( factorial(28) != 304888344611713860501504000000, "Passed!" );
-});
-
-QUnit.test( "Factorial of -1 returns NaN", function( assert ) {
-  assert.ok( isNaN(factorial(-1)), "Passed!" );
-});
-
-QUnit.test( "Factorial of 5.5 returns NaN", function( assert ) {
-  assert.ok( isNaN(factorial(5.5)), "Passed!" );
-});
-
-QUnit.test( "Factorial of -5.5 returns NaN", function( assert ) {
-  assert.ok( isNaN(factorial(-5.5)), "Passed!" );
-});
-
-QUnit.test( 'Factorial of "abcd" returns NaN', function( assert ) {
-  assert.ok( isNaN(factorial("abcd")), "Passed!" );
-});
-
-QUnit.test( 'Factorial of "5abcd6" returns NaN', function( assert ) {
-  assert.ok( isNaN(factorial("5abcd6")), "Passed!" );
-});
-
-QUnit.test( 'Factorial of true returns NaN', function( assert ) {
-  assert.ok( isNaN(factorial(true)), "Passed!" );
-});
-
-QUnit.test( 'Factorial of false returns NaN', function( assert ) {
-  assert.ok( isNaN(factorial(false)), "Passed!" );
-});
-
-QUnit.test( 'Factorial of 5.0 returns 120', function( assert ) {
-  assert.ok( factorial(5.0) == 120, "Passed!" );
+QUnit.test( 'Negative Test Cases - Argument passed is not a number', function( assert ) {
+  assert.ok( isNaN(factorial("abcd")), "Passed! - Factorial of 'abcd' returns NaN" );
+  assert.ok( isNaN(factorial("5abcd6")), "Passed! - Factorial of '5abcd6' returns NaN" );  
+  assert.ok( isNaN(factorial(true)), "Passed! - Factorial of true returns NaN" ); 
+  assert.ok( isNaN(factorial(false)), "Passed! - Factorial of false returns NaN" );
 });
